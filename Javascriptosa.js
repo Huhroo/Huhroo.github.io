a8c97ea539d931d1e190d05a19de6e429f3ff777
Huhroo <roope.v.huhta@gmail.com>
2023-10-18 17:25:23 +0300

    Comments, fixes and Readme update for school

--- Javascriptosa.js
+++ Javascriptosa.js
@@ -10,22 +10,19 @@ function Tool(count, basevalue,value, cost, name) {
     this.name = name;
   }
 
+//creating tools
 const club = new Tool(0,1,0,10,'club');
 const bridge = new Tool(0,5,0,100,'bridge');
 
+//creating arrays that contain some relevant tool info for my table purposes and grading purposes
 const clubInfoArray = generateInfoArray(club);
 const bridgeInfoArray = generateInfoArray(bridge);
 
 
-function updateInfoArray(tool, infoArray) {
-    // Update the existing infoArray in place
-    infoArray[0] = tool.name;
-    infoArray[1] = tool.count;
-    infoArray[2] = tool.value;
-}
-
+// set up some coin related values
 let coinGenerationRate = 1;
 let coinClickPower = 1;
+// I set up coingenerationformula as a global so it's more eesy to expand  and use now and later
 let coinGenerationFormula = (club.value + bridge.value) * coinGenerationRate;
 
 
@@ -42,15 +39,25 @@ const upgrade1Button = document.getElementById('upgrade1');
 const upgrade2Button = document.getElementById('upgrade2');
 const darkmodebutton = document.getElementById('darkmodebutton');
 const lighmodebutton = document.getElementById('lighmodebutton');
-
+//Get API related things, worldtimeapi is an open API that can be used in multiple ways
 const apiUrl = 'https://worldtimeapi.org/api/timezone/Etc/GMT';
 const gmtTimeElement = document.getElementById('gmtTime');
 
+//I call fetch time function here so it gets called early, function is explained later
 fetchTime();
+
+// function that updates the inputed tool array (above), takes in the object (tool) and the relevant array name (e.g clubInfoArray)
+function updateInfoArray(tool, infoArray) { 
+    
+    infoArray[0] = tool.name;
+    infoArray[1] = tool.count;
+    infoArray[2] = tool.value;
+}
+
 
 // Function to update the coin count display
 function updatecoinCount() {  
-     // Check if coinCount is a valid number
+     // Check if coinCount is a valid number, used try,throw catch for grading purposes.
         try {
             if (typeof coinCount !== 'number' || isNaN(coinCount)) {
                 throw new Error("Invalid coinCount value");
@@ -64,8 +71,7 @@ function updatecoinCount() {
         
     
     let formatedcoinCount = 0;
-
-
+    //this if logic makes displaying large numbers more pleasing to a eye
     if (coinCount >= 1000000) {
         formatedcoinCount = (coinCount / 1000000).toFixed(3) + 'm';
     } else if (coinCount >= 1000) {
@@ -73,60 +79,64 @@ function updatecoinCount() {
     } else {
         formatedcoinCount = coinCount.toFixed(0);
     }
+    //some basic DOM manipulation here so we can display the coin information on the html website.
     const resultElement = document.getElementById('coinCount');
     const coinspersecondElement = document.getElementById('coinspersecond');
+    
     resultElement.textContent = `${formatedcoinCount} coins`;
     coinspersecondElement.textContent =`generating ${coinGenerationFormula} coins/s`
 }
 
+//handleClick function makes the on click animation possible 
 function handleClick() {
-    // Add the "clicked" class to trigger the animation
     clickButton.classList.add('clicked');
-
-    // Remove the "clicked" class after a short delay (e.g., 200ms)
+    // Using timeout to remove the "clicked" class after a short delay 
     setTimeout(function () {
         clickButton.classList.remove('clicked');
-    }, 200); // 200ms delay (adjust as needed)
+    }, 200); 
 }
 
-// Event listener for button click
+// Event listener for the button click
 clickButton.addEventListener('click', function () {
     // Increase the coin count by coinClickPower when the button is clicked
     coinCount = coinCount + coinClickPower;
-    // Update the coin count display
+    // function calls to update coin count display and the previous function that makes the animation possible
     handleClick();
     updatecoinCount();
 
 });
 
+//Basic function that updates the coinGenerationFormula
 function updatecoinGenerationFormula(){
 coinGenerationFormula = (club.value + bridge.value) * coinGenerationRate;
 }
 
 
-// Function to buy a specific type of tool
+// Function to buy a specific type of tool, it takes a tool object in and handles it.
 function buyTool(tool) {
-
+    //some basic math logic so you cant buy something if you don't have the coins
     if (coinCount >= tool.cost) {
         coinCount -= tool.cost;
+        // adding 1 tool
         tool.count++;
+        //I used math.round becouse i like it when the numbers are round
         tool.cost = Math.round(tool.cost * 1.1); // Increase cost by 10%
         updatecoinCount();
         updateToolValue(tool);
         updateToolInfo(tool);
         updatecoinGenerationFormula();
         
-
+    // if you don't have the coin game gives you an alert
     } else {
         alert(`You do not have enough coins to buy a ${tool.name}.`);
     }
 }
 
-
+//function similiar to one before this but it tries to buy X amount items, in the current game it is used to buy 10 of something.
 function buyXTool(value, tool) {
     let x = tool.cost;
     let totalCost = 0;
-
+    //for loop where it calculates how much coin would it take to buy x amount of item
     for (let i = 0; i < value; i++) {
         totalCost += x;
         x = Math.round(x * 1.1);
@@ -142,12 +152,14 @@ function buyXTool(value, tool) {
         updatecoinGenerationFormula();
 
     } else {
+        //This alert is similiar to the but it tells you little bit more information that is useful to know.
         alert(`You do not have enough coins to buy ${value} ${tool.name}s, cost is ${Math.round(totalCost)}`);
     }
 }
-
+//function that updates the relevant tools information
 function updateToolInfo(tool) {
     let sectionId = '';
+    //This if loop might seem clunky if you think object oriented way, but i was under a time crunch and it works as a error check in this way. 
     if (tool.name === "club"){
         sectionId="clubSection";
     } else if (tool.name === "bridge"){
@@ -156,21 +168,21 @@ function updateToolInfo(tool) {
         alert(`nothing matched ${tool}`)
         return;
     }
-    
+    //here is some DOM manipulation that updates the relevant tools section
     const section = document.getElementById(sectionId);
     section.querySelector('.buy-button').textContent = `Upgrade ${tool.name}`;
- 
     section.querySelector('.cost').textContent = tool.cost;
-   
     section.querySelector('.count').textContent = tool.count;
     
 }
 
+//Function to buy the first upgrade, upgrades aren't objects at the moment becouse they are quite unique in their effect at the moment
 function buyupgrade1() {
     if (coinCount >= 100) {
         coinCount -= 100;
         updatecoinCount;
         updateClickPower(coinClickPower * 2);
+        //DOM manipulation to remove the upgrade button so it can't be purchased twice
         const upgrade1Button = document.getElementById('upgrade1');
         upgrade1Button.remove()
 
@@ -178,7 +190,7 @@ function buyupgrade1() {
         alert('You do not have enough coins to buy this upgrade.');
     }
 }
-
+//function to buy the second upgrade
 function buyupgrade2() {
     if (coinCount >= 100) {
         coinCount -= 100;
@@ -186,6 +198,7 @@ function buyupgrade2() {
         updateToolBaseValue(club.basevalue * 2,club);
         updateToolValue(club);
         updatecoinGenerationFormula();
+        //DOM manipulation to remove the upgrade button so it can't be purchased twice
         const upgrade2Button = document.getElementById('upgrade2');
         upgrade2Button.remove()
 
@@ -194,49 +207,56 @@ function buyupgrade2() {
     }
 }
 
+//this function check the coin amount and when there are enough collected coins it reveals the upgrade section by removing a class that hides it
 function checkcoinThreshold() {
     if (coinCount >= 10) {
         upgrades.classList.remove('menuhidden');
         
  }
 }
-
+//simple function that updates the coinClickPower to the given value
 function updateClickPower(value){
     coinClickPower = value; 
 }
-
+//Simple function that updates the given tool's value
 function updateToolValue(tool){   
       tool.value = tool.basevalue * tool.count;
 }
 
+//Simple function that updates the tool's base value to the given value and then also calls to update it's value (becouse baseValue affects the value)
 function updateToolBaseValue(value, tool){
     tool.basevalue = value;
     updateToolValue;
 }
 
 
-// Function to generate coins automatically
+// Function to generate coins automatically this gets called 10 times per second by upcoming setInterval
 function generatecoins() {
+    //Becouse i wanted the function to be called 10 per second i must divide the coingenerationformula by 10 to make the math check out
     coinCount += coinGenerationFormula/10;
     updatecoinCount();
-    checkcoinThreshold(); // Check if the threshold is met
+    //here is also called the thershold checker
+    checkcoinThreshold(); 
 }
 
 // Update the coin count display every 0.1 seconds
 setInterval(generatecoins, 100);
 
+//Function that changes the stylesheet to the dark one
 function setdarkmode(){
     document.getElementById('stylesheet1').disabled = false;
     document.getElementById('stylesheet2').disabled = true;
 }
-
+//Function that changes the stylesheet to the light one 
 function setlightmode(){
     document.getElementById('stylesheet1').disabled = true;
     document.getElementById('stylesheet2').disabled = false;
 }
 
+//here is the eventlistener that waits the user to submit their troll's name and by DOM manipulation changes all instances of the word "troll" with submited name
 document.getElementById('nameForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Prevents the form from submitting and refreshing the page
+     // Prevents the form from submitting and refreshing the page
+    event.preventDefault();
     const submittedName = document.getElementById('trollName').value;
     const trollText = document.getElementById('trollText');
     const headerTitle = document.getElementById('headerTitle');
@@ -245,7 +265,7 @@ document.getElementById('nameForm').addEventListener('submit', function(event) {
     document.title = `${submittedName} Clicker`;
 });
 
-// Event listener for each button
+// Event listeners for rest of the buttons , one below is an example of one i had to do differently becouse otherwise those functions get called when ever the website loads 
 buyclubButton.addEventListener('click',function(){
     buyTool(club);
 });
@@ -265,18 +285,21 @@ buy10bridgesbutton.addEventListener('click',function(){
     buyXTool(10,bridge);
 });
 
+//setinterval that updates the time every half a minute, i didn't want to spam too many API calls becouse i feel it's unnice
 setInterval(fetchTime, 30000);
 
-
+//Function that utilizes fetch api call function to get the gmt time to display on the site
 function fetchTime(){
 
     fetch(apiUrl)
+    // .then waits for the api to respond and if it responds function handles the information
       .then(response => response.json())
       .then(data => {
         const currentTime = new Date(data.utc_datetime);
         const formattedTime = currentTime.toLocaleTimeString('en-US', { timeStyle: 'short' });
         gmtTimeElement.textContent = `GMT Time: ${formattedTime}`;
       })
+      //if error occurs it console logs it and changes the time display to API CALL FAILED
       .catch(error =>{
         console.error('Error fetching time:', error)
         gmtTimeElement.textContent = 'API CALL FAILED';
@@ -284,11 +307,11 @@ function fetchTime(){
 
       
 }
-
+ //function that creates a new array utilizing the given tool's information
 function generateInfoArray(tool) {
     return [tool.name, tool.count, tool.value];
 }
-
+//Function that updates the given array's information based on the given object
 function updateInfoArray(tool, infoArray) {
     // Update the existing infoArray in place
     infoArray[0] = tool.name;
@@ -296,8 +319,11 @@ function updateInfoArray(tool, infoArray) {
     infoArray[2] = tool.value;
 }
 
+
+//1 second interval that call the update function below this
 setInterval(updateArraysAndTable,1000);
 
+ // Function that updates the arrays and also updates the table on the website
 function updateArraysAndTable(){
     updateInfoArray(club, clubInfoArray);
     updateInfoArray(bridge, bridgeInfoArray);
@@ -312,6 +338,5 @@ function updateArraysAndTable(){
     document.getElementById('bridgeTValue').textContent = bridgeInfoArray[2];
 
 
-    console.log('clubinfo',clubInfoArray);
-    console.log('bridgeinfo',bridgeInfoArray)
+
 }
--- README.md
+++ README.md
@@ -1,2 +1,190 @@
 # Huhroo.github.io
 My clicker game website project
+
+Troll clicker is an incremental clicker game where you can collect coins by clicking the troll and buying tools.
+You can get more information out of things by hovering different elements (sorry mobile users)
+this is an School project so don't expect breaking game desing
+
+KOULU ARVOSTELIALLE
+Hei! Tämä on minun klikkeri pelivu ja alla on arviointi kriteerit ja kommentini siitä että toteutuuko ne ja mistä ne mahdollisesti löytyy.
+
+HTML (25%)
+1/5:
+Basic HTML structure is present.
+
+-lyhyellä vilkaisulla minun html tiedostoa sanonoisin että kyllä juu.
+ -sivu on jaettu osiin
+ -siellä hyödynnetään kaikkia perus asioita
+
+ 1pt
+
+2/5:
+HTML structure with clear content differentiation (headings, paragraphs, lists).
+
+-Mielestäni sivuni täyttää tämän vaatimuksen
+
+1pt
+
+3/5:
+Use of forms, links, and media.
+
+sivuillani on:
+ forms -> settings napin takana on forms jolla voi vaihtaa peikon nimeä
+ linkki -> "learn about trolls" on linkki wikipediaan 
+ media -> peikko nappi sisältää kuvan
+
+ eli sanoisin että se täyttyy
+
+ 1pt
+
+4/5:
+Tables are effectively used.
+
+Sivuillani on yksi table mutta minusta se ei tarvitse enempää (ja se jopa päivittyy peliä pelatessa) 
+
+sanoisin itse että 1pt
+
+1pt
+
+5/5:
+Consistent use of semantic HTML throughout, ensuring better structure and understanding of the content.
+
+Sivuni on hyödyntää näitä (nav, section jne) mutta se kai on siinä ja siinä että onko niitä käytetty hyvin
+
+hyvänä päivänä 1pt 
+
+CSS (25%)
+1/5:
+Basic CSS styling (colors, fonts).
+
+Kyllä
+
+1pt
+
+2/5:
+Use of classes and IDs to style specific elements.
+
+kyllä 
+
+1pt
+
+3/5:
+Implementation of responsive design elements.
+
+Tämä on väiteltävissä oleva asia, sillä sivuni kyllä mukautuu monille näyttökoille ja omissa testauksissani se on 1980x1200 näytöissä ihan järkevä
+ja toimii myös puhelimilla ja tableteilla ihan järkevästi
+
+antaisin itse 1pt mutta ymmärrän myös 0pt
+
+4/5:
+Use of layouts for advanced user interfaces (arrays, float, flexbox, css grid)
+
+Hyödynnän flexboxxia molemmissa container:ssä
+
+1pt
+
+5/5:
+Styling demonstrates a strong grasp of layout principles, aesthetics, and user experience.
+
+No tämän ymmärrän täysin jos haluaa antaa 0pt mutta mielestäni käyttäjä kokemus on hyvä ja aesteettisesti se on vanhan koulukunnan edustaja josta itse pidän
+
+en ehkä itse antaisi 1pt mutta ehkä kuitenkin se on sen arvoinen? Paha sanoa itse...
+
+JavaScript Basics (25%)
+1/5:
+Simple interactions (like alerts on button click).
+
+Koko projektin perus ominaisuus on napin painaminen joka tekee javascriptillä jotakin
+
+1pt
+
+2/5:
+Multiple event listeners and basic DOM manipulations.
+
+Projektissa on kirjoitus hetkellä 10 event listeneriä ja lukuisia DOM manipulaatioita
+esim kun klikkaat peikkoa näytöllä oleva kolikko laskuri päivittyy.
+
+1pt
+
+3/5:
+Use of arrays, objects, and functions.
+
+Javascriptistä iso osa on object tool:in ympärille rakennettu, funktioita on melkein 20, ja arraytä hyödynnetään luomalla tool's olioiden,
+ sisällöistä array functiolla generateInfoArray(tool) joilla sitten päivitettään jatkuvasti html tablen sisältöä
+
+ 1pt 
+
+4/5:
+Advanced logic, looping through data, and dynamic DOM updates.
+
+"2. Advanced logic, looping through data, and dynamic DOM updates
+
+Tämä kriteeri keskittyy ohjelmointilogiikan kehittyneempiin osa-alueisiin, mukaan lukien:
+
+• Ehtolausekkeet: Käytä ehtoja, kuten if, else if, ja switch, hallitaksesi loogisia päätöksiä.
+
+• Datan iteroiminen: Käytä erilaisia JavaScriptin loopeja (kuten for, for...of, forEach, map, jne.)
+käydäksesi läpi tietorakenteita.
+
+• Dynaamiset DOM-päivitykset: Aktiivinen ja jatkuva interaktio käyttöliittymän kanssa käyttäen
+JavaScriptiä (esim. AJAX, Fetch API) päivittämään, lisäämään tai poistamaan DOM-elementtejä ja
+niiden sisältöä reaaliaikaisesti.
+
+• Käytä Document Object Model (DOM) APIa, kuten getElementById, querySelector, jne.
+päivittääksesi web-sivun sisältöä dynaamisesti."
+
+tämä kai on siinä ja siinä.
+ -DOM manipulaatio on sivulla jatkuvaa ja hyödyntää etenkin getElementbyId:tä monessakin paikassa ja kuivaisin sitä dynaamiseksi (fetch API hakee esim kellon ajan puolen minuutin välein)
+ -looppeja käytän vain "buyXTool()" funktiossa koska  muualla niille ei ollut tässä projektissa käyttöä
+ -Ehtolausekkeita käytin monessa paikkaa esim updatecoinCount() if looppaa rahan läpi ja käsittelee sitä eri tavoin jos rahan määrä on tarpeeksi suurta
+
+ itse sanoisin että juu 1pt
+5/5:
+Consistent use of Object-Oriented JavaScript principles.
+
+tästä toteutuu osa (luokat ja oliot) mutta muuten ei
+
+0pt
+
+Asynchronous Operations (25%)
+1/5:
+Use of timers.
+
+setinterval() käytetään kolme kertaa ja on perus palikka sivustossani
+ja handleClick() hyödyntää settimeout ajastinta
+
+1pt
+
+2/5:
+Successful implementation of an AJAX call or Fetch.
+
+Sivuni onnistuneesti hyuödyntää Fetchiä ja hakee nykyisen gmt ajan. (javascript fetchTime())
+
+1pt
+
+3/5:
+Data from the asynchronous call is displayed on the webpage.
+
+fetchTime() asynkronisesti hakee ajan ja se näkyy minun sivun navigaatio baarissa
+
+1pt
+
+4/5:
+Error handling is implemented (for failed API calls, etc.).
+
+noh osittain
+fetchTime() antaa virheilmoituksen jos API kutsu epäonnistuu, jota tuli myös testattua onnistuneesti kun github pages oli yhteistyö haluton (sivu josta api tulee oli merkattu koodissa HTTP sivuna vaikka se on oikeasti HTTPS, githubpages ei anna sivun tehdä yhteyttä jos yhteen otettava sivu on merkattu HTTP sivuksi vaikka se oikeasti on HTTPS)
+
+ja funktio updatecoinCount() testaa onko annettu arvo oikea hyödyntäen try,throw catch metodeja(?)
+
+eli error handling on implementoitu ainaki joillekin asioille
+1pt
+
+5/5:
+Effective use of asynchronous data to enhance user experience (like filtering, sorting).
+
+tästä en osaa itse sanoa fetchTime() funktio on toteutettu asynkronisia keinoja hyödyntäen mutta riittääkö yksi asia pisteeseen tässä?
+
+LOPPUPISTEET
+Itselle antaisin reallistisesti 14 pistettä.
+
